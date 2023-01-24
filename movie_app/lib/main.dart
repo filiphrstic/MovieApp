56e799c38f0bee18ca43b6b49e00e6b4409ea1d2
filip.hrstic <filip.hrstic@prospekt.hr>
2023-01-24 22:51:48 +0100

    feat - added themes file

--- movie_app/lib/main.dart
+++ movie_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/utilities/themes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,18 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+      theme: Themes.themeData,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -95,12 +85,14 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
+            Text(
               'You have pushed the button this many times:',
+              style: Theme.of(context).textTheme.headline2,
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              // style: Theme.of(context).textTheme.bodyLarge,
+              style: TextStyle(fontSize: 20),
             ),
           ],
         ),
---
+++ movie_app/lib/utilities/themes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Colors.green;
+
+class Themes {
+  static ThemeData themeData = ThemeData(
+    colorSchemeSeed: primaryColor,
+  );
+}
